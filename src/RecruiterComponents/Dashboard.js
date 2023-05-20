f4b39e81c18453aa5fbf0e56fa563a880f72334a
AkyiTheFirst <kkhin2001@gmail.com>
2023-05-20 18:05:17 +0630

    built: job css file

--- src/RecruiterComponents/Dashboard.js
+++ src/RecruiterComponents/Dashboard.js
@@ -9,32 +9,34 @@ const Dashboard = () => {
         <div className="RecruiterDashboard">
             <Header/>
             <div className="RecruiterProfile">
-                <div className="RecruiterName">
-                    <img src={profileIcon} alt="" />
-                    <h3>Name</h3>
+                    <div className = "wrapper">
+                    <div className="RecruiterName">
+                        <img className = "orgProfile" src={profileIcon} alt="" />
+                        <h3>Name</h3>
+                    </div>
+                    
+                    <div className="RecruiterDetails">
+                        <h3>Organization Type : </h3>
+                        <p>demo text</p>
+
+                        <h3>Job Position : </h3>
+                        <p>demo text</p>
+
+                        <h3>Salary Range : </h3>
+                        <p>demo text</p>
+
+                        <h3>Working Hours : </h3>
+                        <p>demo text</p>
+
+                        <h3>Benefits : </h3>
+                        <p>demo text</p>
+
+                        <h3>Required Skills : </h3>
+                        <p>demo text</p>
+                    </div>
+
+                    <Link className = "buttons" to="/recruiter/job">See your jobs</Link>
                 </div>
-                
-                <div className="RecruiterDetails">
-                    <h3>Organization Type</h3>
-                    <p>demo text</p>
-
-                    <h3>Organization Type</h3>
-                    <p>demo text</p>
-
-                    <h3>Organization Type</h3>
-                    <p>demo text</p>
-
-                    <h3>Organization Type</h3>
-                    <p>demo text</p>
-
-                    <h3>Organization Type</h3>
-                    <p>demo text</p>
-
-                    <h3>Organization Type</h3>
-                    <p>demo text</p>
-                </div>
-
-                <Link to="/recruiter/job">See your jobs</Link>
             </div>
         </div>
     )
