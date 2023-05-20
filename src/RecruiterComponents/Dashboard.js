a62eaf9e614bc813296d6869bf653f732698e785
Yato <yatoakii@gmail.com>
2023-05-21 00:29:36 +0630

    feat: can render Dashboard with data

--- src/RecruiterComponents/Dashboard.js
+++ src/RecruiterComponents/Dashboard.js
@@ -2,8 +2,12 @@ import "./Dashboard.css"
 import Header from "../UtilityComponents/Header"
 import profileIcon from "../assets/profileIcon.avif"
 import { Link } from "react-router-dom"
+import { useSelector } from "react-redux"
+
 
 const Dashboard = () => {
+
+    const user = useSelector(state => state.userReducer)
 
     return(
         <div className="RecruiterDashboard">
@@ -12,27 +16,21 @@ const Dashboard = () => {
                     <div className = "wrapper">
                     <div className="RecruiterName">
                         <img className = "orgProfile" src={profileIcon} alt="" />
-                        <h3>Name</h3>
+                        <h3>{user.data.username}</h3>
                     </div>
                     
                     <div className="RecruiterDetails">
                         <h3>Organization Type : </h3>
-                        <p>demo text</p>
+                        <p>{user.data.companySize}</p>
 
-                        <h3>Job Position : </h3>
-                        <p>demo text</p>
+                        <h3>Location : </h3>
+                        <p>{user.data.currentLocation}</p>
 
-                        <h3>Salary Range : </h3>
-                        <p>demo text</p>
+                        <h3>linkedin : </h3>
+                        <p>{user.data.linkedin}</p>
 
-                        <h3>Working Hours : </h3>
-                        <p>demo text</p>
-
-                        <h3>Benefits : </h3>
-                        <p>demo text</p>
-
-                        <h3>Required Skills : </h3>
-                        <p>demo text</p>
+                        <h3>Website : </h3>
+                        <p>{user.data.website}</p>
                     </div>
 
                     <Link className = "buttons" to="/recruiter/job">See your jobs</Link>
