81a6449f2286b8e13d5aced7a93fa3653bd06f45
Yato <yatoakii@gmail.com>
2023-05-20 14:10:22 +0630

    fix: explore recruiter btn missings

--- src/RecruiterComponents/Explore.js
+++ src/RecruiterComponents/Explore.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import Header from "../UtilityComponents/Header"
-import Logo from "../assets/Logo.png"
+import profileIcon from "../assets/profileIcon.avif"
 import "./Explore.css"
+import { Link } from "react-router-dom";
 
 const Explore = () => {
   
@@ -11,27 +12,30 @@ const Explore = () => {
   return (
     <div className="RecruiterExplore">
       <Header/>
+      <Link>Post</Link>
+      <Link>Go to Dashboard</Link>
+      <p>Update you Requirements to get a perfect employee for you</p>
       <div className="jobCards">
         <div className="jobCard">
-          <img src={Logo} alt="" />
+          <img src={profileIcon} alt="" />
           <h1>Name</h1>
           <p>This will be bio here</p>
         </div>
 
         <div className="jobCard">
-          <img src={Logo} alt="" />
+          <img src={profileIcon} alt="" />
           <h1>Name</h1>
           <p>This will be bio here</p>
         </div>
 
         <div className="jobCard">
-          <img src={Logo} alt="" />
+          <img src={profileIcon} alt="" />
           <h1>Name</h1>
           <p>This will be bio here</p>
         </div>
 
         <div className="jobCard">
-          <img src={Logo} alt="" />
+          <img src={profileIcon} alt="" />
           <h1>Name</h1>
           <p>This will be bio here</p>
         </div>
