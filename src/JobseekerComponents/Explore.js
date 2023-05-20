0a6d45243d97d133250516de643f12cc352d984e
Yato <yatoakii@gmail.com>
2023-05-20 14:21:41 +0630

    build: add explore jobseeker

--- src/JobseekerComponents/Explore.js
+++ src/JobseekerComponents/Explore.js
@@ -1,14 +1,39 @@
 import React from "react";
-import { getAuth } from "firebase/auth";
+import profileIcon from "../assets/profileIcon.avif"
+import Header from "../UtilityComponents/Header";
+import { Link } from "react-router-dom";
+import "./Explore.css"
 
 const Explore = () => {
-  const auth = getAuth();
-  const user = auth.currentUser;
-  console.log(user)
+  
 
   return (
-    <div>
-      {user ? <p>User is logged in</p> : <p>User is not logged in</p>}
+    <div className="jobseekerExplore">
+      <Header/>
+      <div className="learningProgress">
+        <h2>Your progress</h2>
+        <progress value={10} max="100"></progress>
+        <Link>Continue Learning</Link>
+      </div>
+      <div className="jobCards">
+        <div className="jobCard">
+          <img src={profileIcon} alt="" />
+          <h1>Name</h1>
+          <p>This will be bio here</p>
+        </div>
+
+        <div className="jobCard">
+          <img src={profileIcon} alt="" />
+          <h1>Name</h1>
+          <p>This will be bio here</p>
+        </div>
+
+        <div className="jobCard">
+          <img src={profileIcon} alt="" />
+          <h1>Name</h1>
+          <p>This will be bio here</p>
+        </div>
+      </div>
     </div>
   );
 };
