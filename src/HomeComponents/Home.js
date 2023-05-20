8b77f87e1f51b2b115e97561d6911de960f60324
Yato <yatoakii@gmail.com>
2023-05-20 16:23:41 +0630

    feat: skip login if already logged in

--- src/HomeComponents/Home.js
+++ src/HomeComponents/Home.js
@@ -2,9 +2,11 @@ import "./Home.css"
 import Header from "../UtilityComponents/Header"
 import Slider from "./Slider"
 import { Link } from "react-router-dom"
+import { useSelector } from "react-redux"
 
 const Home = () => {
 
+    const user = useSelector(state => state.authReducer)
     return(
         <div className="Home">
             <Header/>
@@ -14,13 +16,28 @@ const Home = () => {
                 <div className="homeNav">
                     <h3>Are you looking for a</h3>
                     <div className="homeBtns">
-                        <Link to="/jobseeker/login" className="homeBtn">
-                            <span>Job</span>
-                        </Link>
+                        {user ? 
+                        <>
+                            <Link to="/jobseeker" className="homeBtn">
+                                <span>Job</span>
+                            </Link>
 
-                        <Link to="/recruiter/login" className="homeBtn">
-                            <span>Qualified Professonal</span>
-                        </Link>
+                            <Link to="/recruiter" className="homeBtn">
+                                <span>Qualified Professonal</span>
+                            </Link>
+                        </>
+                        :
+                        <>
+                            <Link to="/jobseeker/login" className="homeBtn">
+                                <span>Job</span>
+                            </Link>
+
+                            <Link to="/recruiter/login" className="homeBtn">
+                                <span>Qualified Professonal</span>
+                            </Link>
+                        </>
+                        }
+                        
                     </div>
                 </div>
                 
