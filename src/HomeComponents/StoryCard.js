789368e75d42b4f4f1e07f7db18bbd4f2e1ee747
Yato <yatoakii@gmail.com>
2023-05-21 13:52:40 +0630

    fix: profile pic

--- src/HomeComponents/StoryCard.js
+++ src/HomeComponents/StoryCard.js
@@ -1,11 +1,12 @@
 import "./StoryCard.css"
 import Logo from "../assets/Logo.png"
+import profileIcon from "../assets/profileIcon.avif"
 
 const StoryCard = () => {
 
     return(
         <div className="storyCard">
-            <img src={Logo} alt="" />
+            <img src={profileIcon} alt="" />
             <h3>Kaung Nay Lin Khant</h3>
             <p>User story. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
         </div>
