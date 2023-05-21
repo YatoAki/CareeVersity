a58cc8eed4a9f2f03a73f1da2b842c0b7d1875b7
Yato <yatoakii@gmail.com>
2023-05-21 08:01:19 +0630

    build: can apply jobs

--- src/JobseekerComponents/Job.js
+++ src/JobseekerComponents/Job.js
@@ -1,76 +1,101 @@
-import "./Job.css"
-import Header from "../UtilityComponents/Header"
-import profileIcon from "../assets/profileIcon.avif"
-import Slider from "./Slider"
+import "./Job.css";
+import Header from "../UtilityComponents/Header";
+import profileIcon from "../assets/profileIcon.avif";
+import Slider from "./Slider";
 import { useParams } from 'react-router-dom';
-import { collection, getDocs, query, where } from "@firebase/firestore";
+import { collection, doc, getDoc, updateDoc } from "@firebase/firestore";
 import db from '../Firebase';
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Job = () => {
+  const { id } = useParams();
+  const auth = useSelector(state => state.authReducer);
 
-    const { id } = useParams();
+  const [data, setData] = useState(null);
 
-    const [data, setData] = useState([]);
+  const handleApply = async () => {
+    try {
+      console.log(id); // Check the value of id
+      const docRef = doc(db, "job", id);
+      const docSnapshot = await getDoc(docRef);
+      const jobData = docSnapshot.data();
+      console.log(jobData);
+      
+      const appliedIds = jobData.applied || []; // Get existing applied ids or initialize as empty array
+      const updatedIds = [...new Set([...appliedIds, auth.user])]; // Append uid to existing applied ids
+      console.log(updatedIds);
+      
+      await updateDoc(docRef, { applied: updatedIds });
 
-    useEffect(() => {
-      loadData();
-    }, []); // Empty dependency array ensures the effect runs only once, similar to componentDidMount
-  
-    const loadData = async () => {
-        try {
-          console.log(id); // Check the value of id
-          const q = query(collection(db, "job"), where("uid", "==", id));
-          const querySnapshot = await getDocs(q);
-          const fetchedData = querySnapshot.docs.map((doc) => doc.data());
-          console.log(fetchedData);
-          setData(fetchedData[0]);
-        } catch (error) {
-          console.error("Error fetching job data:", error);
-        }
-      };
+      setData((prevData) => ({ ...prevData, applied: updatedIds }));
+      console.log(data);
+    } catch (error) {
+      console.error("Error updating job data:", error);
+    }
+  };
 
-    return(
-        <div className="Job">
-            <Header/>
-            <div className="jobCard">
-                <img src={profileIcon} alt="" />
-                <h3 className="title">{data.username}</h3>
-                <div className="jobDetails">
-                    <h3>Organization Type : </h3>
-                    <p>{data.companySize}</p>
+  useEffect(() => {
+    loadData();
+  }, []); // Empty dependency array ensures the effect runs only once, similar to componentDidMount
 
-                    <h3>Job Position :</h3> 
-                    <p>{data.jobTitle}</p>
+  const loadData = async () => {
+    try {
+      console.log(id); // Check the value of id
+      const docRef = doc(db, "job", id);
+      const docSnapshot = await getDoc(docRef);
+      const jobData = docSnapshot.data();
+      console.log(jobData);
+      setData(jobData);
+    } catch (error) {
+      console.error("Error fetching job data:", error);
+    }
+  };
 
-                    <h3>Salary Range :</h3>
-                    <p>{data.salaryRange}</p>
+  if (!data) {
+    return <div>Loading...</div>;
+  }
 
-                    <h3>Working Hour : </h3>
-                    <p>{data.workingHours}</p>
+  return (
+    <div className="Job">
+      <Header />
+      <div className="jobCard">
+        <img src={profileIcon} alt="" />
+        <h3 className="title">{data.username}</h3>
+        <div className="jobDetails">
+          <h3>Organization Type:</h3>
+          <p>{data.companySize}</p>
 
-                    <h3>Benefits : </h3>
-                    <p>{data.benefits}</p>
+          <h3>Job Position:</h3>
+          <p>{data.jobTitle}</p>
 
-                    <h3>Required Skills : </h3>
-                    <ul>
-                        {data.selectedTech ? data.selectedTech.map((name) => {
-                            return(
-                                <li>{name.label}</li>
-                            )
-                        }): null
-                    }
-                    </ul>
-                </div>
-                <div className="requiredCourses">
-                    <h2>Required Course</h2>
-                    <Slider/>
-                </div>
-                <Link to="/jobseeker">Back to explore</Link>
-            </div>
+          <h3>Salary Range:</h3>
+          <p>{data.salaryRange}</p>
+
+          <h3>Working Hour:</h3>
+          <p>{data.workingHours}</p>
+
+          <h3>Benefits:</h3>
+          <p>{data.benefits}</p>
+
+          <h3>Required Skills:</h3>
+          <ul>
+            {data.selectedTech &&
+              data.selectedTech.map((name) => <li key={name.value}>{name.label}</li>)}
+          </ul>
+
+          <h3>Apply:</h3>
+          <button onClick={handleApply}>Apply</button>
         </div>
-    )
-}
+        <div className="requiredCourses">
+          <h2>Required Course</h2>
+          <Slider />
+        </div>
+        <Link to="/jobseeker">Back to explore</Link>
+      </div>
+    </div>
+  );
+};
 
-export default Job
+export default Job;
